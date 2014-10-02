29c5823cd5089ef2ca64d7f29867bd24a209dc2a
Eason Lin <easonylin@gmail.com>
2014-10-02 23:05:24 +0000

    meh

--- wall-app.rb
+++ wall-app.rb
@@ -9,6 +9,8 @@ class Message
   property :id,         Serial
   property :body,       Text,     required: true
   property :created_at, DateTime, required: true
+  property :votes,      Integer,  required: true, default: 0
+  property :downvotes,  Integer,  required: true, default: 0
 end
 
 DataMapper.finalize()
@@ -19,15 +21,45 @@ get("/") do
   erb(:index, locals: { messages: records })
 end
 
+get '/reverse' do
+  records = Message.all(reverse_order: :created_at.desc)
+  erb(:index, locals: { messages: records })
+end
+
 post("/messages") do
-  message_body = params["body"]
-  message_time = DateTime.now
-
-  message = Message.create(body: message_body, created_at: message_time)
-
-  if message.saved?
-    redirect("/")
+  if Message.empty?
+    puts "DON'T GIVE ME AN EMPTY POST, SCUMBAG!!!"
+    redirect to('/')
   else
-    erb(:error)
+    message_body = params["body"]
+    message_time = DateTime.now
+
+    message = Message.create(body: message_body, created_at: message_time)
+
+    if message.saved?
+      redirect("/")
+    else
+      erb(:error)
+    end
   end
+end
+
+post "/messages/:id/upvote" do
+  post = Message.where(:id => params[:id])
+  post.votes += 1
+  post.save
+  redirect to('/')
+end
+
+post "/messages/:id/downvote" do
+  post = Message.where(:id => params[:id])
+  post.downvotes += 1
+  post.save
+  redirect to('/')
+end
+
+post "/messages/:id/delete" do
+  post = Message.where(:id => params[:id])
+  post.delete
+  redirect to('/')
 end
